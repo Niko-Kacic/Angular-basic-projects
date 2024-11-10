81006b8e9653ac2fb19d2f5050b29a6134e30c3b
NikoBoolean <niko.kacic@gmail.com>
2024-11-09 23:48:53 -0300

    Adding basic methods

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -11,5 +11,19 @@ export class HeroComponent {
   public name: string = 'ironman';
   public age: number = 45;
 
+  get capitalizedName():string {
+    return this.name.toUpperCase();
+  }
 
+  getHeroDescription():string{
+    return `${this.name} - ${this.age}`;
+  }
+
+  changeHeroName():void {
+    this.name = 'Bucky';
+  }
+
+  changeHeroAge():void {
+    this.age = 30;
+  }
 }
